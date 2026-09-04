50d607bcfc27e30c9e7a1108d45668af3b803d49
Ruby Gupta <guptaruubyy013@gmail.com>
2026-09-04 22:42:34 +0530

    Fix GitHub Pages deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dermcare-clinic-management-system">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
